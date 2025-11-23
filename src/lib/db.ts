c3e856605cec1f2681cfdfcaea3539a9e1c2ce28
ade.arwans <ade.arwans@gmail.com>
2025-11-23 04:34:10 +0000

    memindahkan output prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'; 
 import { PrismaPg } from '@prisma/adapter-pg' 
